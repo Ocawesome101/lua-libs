9bc3572770af9b3f185043a4288b7b60504ca8b0
Ocawesome101 <ocawesome101@gmail.com>
2020-09-03 00:10:42 -0400

    aaaaaaa

--- lib/tui.lua
+++ lib/tui.lua
@@ -8,8 +8,8 @@ local function pad(s, w)
   return s .. (" "):rep(w - #s)
 end
 
-local function fill(tx, ty, w, h)
-  local ln = term.color(pad(" ", w), nil, term.colors.bg.white)
+local function fill(tx, ty, w, h, c)
+  local ln = term.color(pad(" ", w), nil, c or term.colors.bg.bright.white)
   for y=ty, ty+h, 1 do
     term.cursor(tx, y)
     io.write(ln)
@@ -20,7 +20,7 @@ local w, h = term.getSize()
 local function draw(items, title, hlt, sel)
   fill(2, 2, w - 4, h - 4)
   term.cursor(3, 3)
-  io.write(term.color(title, term.colors.fg.black, term.colors.bg.white))
+  io.write(term.color(title, term.colors.fg.black, term.colors.bg.bright.white))
   for i=1, #items, 1 do
     local text = pad(items[i].text, w - 10)
     if items[i].selectable then
@@ -35,7 +35,7 @@ local function draw(items, title, hlt, sel)
     if hlt == i then
       text = term.color(text, term.colors.fg.bright.white, term.colors.bg.red)
     else
-      text = term.color(text, term.colors.fg.black, term.colors.bg.white)
+      text = term.color(text, term.colors.fg.black, term.colors.bg.bright.white)
     end
     term.cursor(3, i + 4)
     io.write(text)
@@ -66,6 +66,84 @@ function tui.menu(items, title)
       return selected
     end
   end
+end
+
+function tui.menuAdvanced(tstruct)
+  assert(type(tstruct) == "table", "bad argument #1 (table expected)")
+  local function init(t)
+    local r = {}
+    for k, v in pairs(t) do
+      if type(v) == "table" then
+        r[k] = {
+          text = v.text,
+          items = init(v.items),
+          parent = r
+        }
+      else
+        r[k] = {
+          text = v,
+          selected = false
+        }
+      end
+    end
+    return r
+  end
+  local struct = init(tstruct)
+  local cur = {items = struct}
+  fill(1, 1, w, h, term.colors.bg.blue)
+  local sel = 1
+  local function draw()
+    fill(3, 3, w - 4, h - 4)
+    local it = cur.items
+    for i=1, #it, 1 do
+      local text = it[i].text
+      if it[i].selected then
+        text = "[*] " .. text
+      elseif it[i].items then
+        text = "--> " .. text
+      else
+        text = "[ ] " .. text
+      end
+      if sel == i then
+        text = term.color(text, term.colors.fg.bright.white, term.colors.bg.red)
+      else
+        text = term.color(text, term.colors.bg.white, term.colors.fg.black)
+      end
+      term.cursor(5, i + 5)
+      io.write(text)
+    end
+    term.cursor(5, h)
+    io.write(term.color("^/v navigate | -> enter submenu | <- exit submenu | <enter> select", term.colors.fg.bright.white, term.colors.bg.blue))
+  end
+  while true do
+    draw()
+    local key = term.getKey()
+    if key == "up" then
+      if sel > 1 then sel = sel - 1 else sel = #cur.items end
+    elseif key == "down" then
+      if sel < #cur.items then sel = sel + 1 else sel = 1 end
+    elseif key == "\13" then
+      if not cur.items[sel].items then
+        cur.items[sel].selected = not cur.items[sel].selected
+      else
+        cur = cur.items[sel]
+      end
+    elseif key == "right" then
+      if cur[sel].items then
+        cur = cur[sel]
+        sel = 1
+      end
+    elseif key == "left" then
+      if cur.parent then
+        cur = cur.parent
+        sel = 1
+      else
+        break
+      end
+    end
+  end
+
+  return struct
 end
 
 return tui
